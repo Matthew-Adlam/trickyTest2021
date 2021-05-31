72d0007f0484fad104c7ab1428abceae184de0db
Matthew Adlam <mattadlam3@gmail.com>
2021-05-31 13:23:49 +1200

    Initial Content:
    Home screen is nearly complete - animation and buttons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,21 +29,108 @@ namespace trickyTest2021
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelGame = new System.Windows.Forms.Panel();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.highScoresBtn = new System.Windows.Forms.Button();
+            this.instructBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.homeLbl = new System.Windows.Forms.Label();
+            this.panelGame.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panelGame
+            // 
+            this.panelGame.BackColor = System.Drawing.Color.White;
+            this.panelGame.Controls.Add(this.homeLbl);
+            this.panelGame.Controls.Add(this.exitBtn);
+            this.panelGame.Controls.Add(this.instructBtn);
+            this.panelGame.Controls.Add(this.highScoresBtn);
+            this.panelGame.Controls.Add(this.startBtn);
+            this.panelGame.Location = new System.Drawing.Point(0, 1);
+            this.panelGame.Name = "panelGame";
+            this.panelGame.Size = new System.Drawing.Size(1332, 703);
+            this.panelGame.TabIndex = 0;
+            this.panelGame.Paint += new System.Windows.Forms.PaintEventHandler(this.panelGame_Paint);
+            this.panelGame.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelGame_MouseMove);
+            // 
+            // startBtn
+            // 
+            this.startBtn.BackColor = System.Drawing.Color.Red;
+            this.startBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startBtn.Location = new System.Drawing.Point(437, 127);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(320, 92);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = false;
+            // 
+            // highScoresBtn
+            // 
+            this.highScoresBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.highScoresBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highScoresBtn.Location = new System.Drawing.Point(437, 265);
+            this.highScoresBtn.Name = "highScoresBtn";
+            this.highScoresBtn.Size = new System.Drawing.Size(320, 92);
+            this.highScoresBtn.TabIndex = 1;
+            this.highScoresBtn.Text = "High Scores";
+            this.highScoresBtn.UseVisualStyleBackColor = false;
+            // 
+            // instructBtn
+            // 
+            this.instructBtn.BackColor = System.Drawing.Color.Lime;
+            this.instructBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructBtn.Location = new System.Drawing.Point(437, 401);
+            this.instructBtn.Name = "instructBtn";
+            this.instructBtn.Size = new System.Drawing.Size(320, 92);
+            this.instructBtn.TabIndex = 2;
+            this.instructBtn.Text = "Instructions";
+            this.instructBtn.UseVisualStyleBackColor = false;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.BackColor = System.Drawing.Color.Blue;
+            this.exitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.Location = new System.Drawing.Point(437, 532);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(320, 92);
+            this.exitBtn.TabIndex = 3;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = false;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // homeLbl
+            // 
+            this.homeLbl.AutoSize = true;
+            this.homeLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.homeLbl.Location = new System.Drawing.Point(433, 17);
+            this.homeLbl.Name = "homeLbl";
+            this.homeLbl.Size = new System.Drawing.Size(324, 69);
+            this.homeLbl.TabIndex = 4;
+            this.homeLbl.Text = "Tricky Test";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1332, 703);
+            this.Controls.Add(this.panelGame);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelGame.ResumeLayout(false);
+            this.panelGame.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelGame;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button instructBtn;
+        private System.Windows.Forms.Button highScoresBtn;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Label homeLbl;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,9 +17,67 @@ namespace trickyTest2021
             InitializeComponent();
         }
 
+        // variables decleration
+
+        public bool inGame = false;
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            homeScreen();
+        }
 
+        private void panelGame_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// The home screen animations, only work if user is not in game, i.e. on home screen
+        /// </summary>
+        private void panelGame_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(inGame == false)
+            {
+                Random colorRnd = new Random();
+                Random sizeRnd = new Random();
+                int size = 50;
+                size = sizeRnd.Next(5, 15);
+                Color randColor = Color.FromArgb(colorRnd.Next(256), colorRnd.Next(256), colorRnd.Next(256));
+                Graphics homeScreen = panelGame.CreateGraphics();
+                Pen pen1 = new Pen(randColor, 2);
+                SolidBrush br = new SolidBrush(randColor);
+                homeScreen.FillEllipse(br, e.X + sizeRnd.Next(-10, 10), e.Y + sizeRnd.Next(-10, 10), size, size);
+                homeScreen.DrawEllipse(pen1, e.X + sizeRnd.Next(-10, 10), e.Y + sizeRnd.Next(-10, 10), size, size);
+
+            }
+        }
+        /// <summary>
+        /// the code to exit the game, confirm that the user wants to
+        /// </summary>
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            // confirms exit, if yes is pressed closes the form
+            string question = "Do you want to exit this program?";
+            string title = "Exit Program";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(question, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        /// <summary>
+        /// makes everything visible/invisible etc that is needed. this is called as a function to save space and time
+        /// </summary>
+        public void homeScreen()
+        {
+            inGame = false;
+
+            exitBtn.Visible = true;
+            instructBtn.Visible = true;
+            startBtn.Visible = true;
+            highScoresBtn.Visible = true;
+            homeLbl.Visible = true;
         }
     }
 }
